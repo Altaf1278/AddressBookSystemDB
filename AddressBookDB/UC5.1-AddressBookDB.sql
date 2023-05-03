aadba8c344b38797f369b92ccffcc75029c65628
altaf <altaf98khan66@gmail.com>
2023-05-03 21:12:51 +0530

    Add: To delete a contact info using name in a table.

--- AddressBookDB/UC5.1-AddressBookDB.sql
+++ AddressBookDB/UC5.1-AddressBookDB.sql
@@ -12,7 +12,9 @@ describe addressbook;
 insert into addressbook(first_name, last_name,  address, city, state, zip,email, phonenumber) values ('Altaf', 'Khan',  'Panvel', 'Mumbai', 'Maharashtra', '410222','ak@123','9665566123'),('Rauf','Majgaokar','Mumbra','Thane','Maharashtra','420569','rauf@gmail.com','9866532214'),('Kiran','urshil','Bajajnagar','Aurangabad','Maharashtra','423568','kirrran@yahoo.com','8956274130'),('Muskan','Siddiqui','Pimpri','Pune','Maharashtra','452698','muski@gmail990','8554956765'),('Priya','Sharma','Udaypur','Kolkata','WestBengal','458997','priya@sharma@123','7658922444'),('Rose','Mir','Dilawarpur','Munger','Kashmir','456988','roserose@34166gmail.com','8963324178'),('ShlokBabu','Kumar','kalyugnagar','Siwan','Bihar','562887','kumarji@789','8553247169');
 select * from addressbook;
 ------  UC4  ------
- update  addressbook set email = 'RoseMir34@gmail.com', phonenumber = '77756898644', address = 'Mirzapur', city = 'Bhopal', state = 'MP', zip = '886004' where first_name = 'Rose' and last_name = 'Mir';
- select * from addressbook;
+update  addressbook set email = 'RoseMir34@gmail.com', phonenumber = '77756898644', address = 'Mirzapur', city = 'Bhopal', state = 'MP', zip = '886004' where first_name = 'Rose' and last_name = 'Mir';
+select * from addressbook;
 
- 
+ ------  UC5  ------
+delete from addressbook where first_name = 'Priya';
+select * from addressbook;  
